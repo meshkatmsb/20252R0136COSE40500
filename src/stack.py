b8ed413faa6742706840058e866cf7c486f215f1
Meshkat Mesbah <meshkatmesbah@Meshkats-MacBook-Pro.local>
2025-12-18 19:27:46 +0900

    Implement stack data structure using class

---
+++ src/stack.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+s = Stack()
+s.push(10)
+s.push(20)
+print(s.pop())
